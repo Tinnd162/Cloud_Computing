458b7f43a04c96846be67ce09f5fe935e160216b
Tinnd162 <tinnd162@gmail.com>
2021-06-12 12:57:24 +0700

    add db LMS

---
+++ lms-db/lms.sql
@@ -0,0 +1,208 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `faculty`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`name` NVARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `faculty` (`id`, `name`) VALUES
+('FIT','Cong nghe thong tin'),
+('FFL','Ngoai ngu'),
+('FCE','Xay dung'),
+('ITE','Vien su pham ky thuat'),
+('FPI','Ly luan chinh tri'),
+('FAS','Khoa ung dung'),
+('FEEE','Dien - dien tu'),
+('FE','Kinh te'),
+('FGRAM','In va truyen thong'),
+('FHQ','Dao tao chat luong cao'),
+('FGTFD','Cong nghe may va thoi trang'),
+('FCFT','Cong nghe hoa hoc va thuc pham'),
+('FME','Co khi che tao may'),
+('FAE','Co khi dong luc');
+
+
+CREATE TABLE `class`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`name` NVARCHAR(50),
+	`major` NVARCHAR(50),
+	`id_faculty` VARCHAR(20),
+	FOREIGN KEY (`id_faculty`) REFERENCES `faculty`(`id`) ON UPDATE CASCADE ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `class` (`id`, `name`, `major`, `id_faculty`) VALUES
+('1','181330','Ky thuat du lieu', 'FIT'),
+('2','18111A','Cong nghe phan mem', 'FIT'),
+('3','181110B','He thong thong tin', 'FIT'),
+('4','181110C','An ninh mang', 'FIT');
+
+
+CREATE TABLE `user`
+(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`first_name` NVARCHAR(20),
+	`last_name` NVARCHAR(20),
+	`middle_name` NVARCHAR(20),
+	`phone_no` VARCHAR(20),
+	`sex` BIT,
+	`dob` DATE,
+	`mail` VARCHAR(50),
+	`password` VARCHAR(50),
+	`class_id` VARCHAR(20),
+	FOREIGN KEY (`class_id`) REFERENCES `class`(`id`) ON UPDATE CASCADE ON DELETE SET NULL,
+	`faculty_id` VARCHAR(20),
+	FOREIGN KEY (`faculty_id`) REFERENCES `faculty`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `user`(`id`, `first_name`,`last_name`,`middle_name`,`phone_no`,`sex`,`dob`,`mail`,`password`,`class_id`,`faculty_id`) VALUES
+('U00001','Viet', 'Lam', 'Hoang', '0863233886',1,'20000515', '18133062@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','1','FIT'),
+('U00002','Ngoc', 'Dinh', 'Quang', '0863233886', 1, '20000825', '18133034@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','1','FIT'),
+('U00003','Hung', 'Pham', 'Ngoc', '0863233886', 1, '20001004', '18133017@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '1','FIT'),
+('U00004','Tin', 'Nguyen', 'Dang Phuoc', '0863233886', 1, '20000216', '18133056@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '1','FIT'),
+('U00005','Thang', 'Pham', 'Viet', '0863233886', 1, '20001201', '18133052@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '1','FIT'),
+('U00006','A', 'Van', 'Nguyen', '0863233886',1,'20000515', '18133062@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','2','FIT'),
+('U00007','B', 'Thi', 'Tran', '0863233886', 1, '20000825', '18133034@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','2','FIT'),
+('U00008','C', 'Dinh', 'Thanh', '0863233886', 1, '20001004', '18133017@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '2','FIT'),
+('U00009','D', 'Nguyen', 'Thi', '0863233886', 1, '20000216', '18133056@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '2','FIT'),
+('U00010','E', 'Hoang', 'Pham', '0863233886', 1, '20001201', '18133052@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '3','FIT'),
+('U00011','F', 'Xuan', 'Phan', '0863233886',1,'20000515', '18133062@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','3','FIT'),
+('U00012','J', 'Ngoc', 'Nguyen', '0863233886', 1, '20000825', '18133034@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a','3','FIT'),
+('U00013','Q', 'Van', 'Dinh', '0863233886', 1, '20001004', '18133017@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '4','FIT'),
+('U00014','K', 'Ngoc', 'Tran', '0863233886', 1, '20000216', '18133056@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '4','FIT'),
+('U00015','H', 'Quang', 'Pham', '0863233886', 1, '20001201', '18133052@student.hcmute.edu.vn','2d944828fdb3cd3bc07c42d6b685333a', '4','FIT');
+
+
+CREATE TABLE `role`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`role` VARCHAR(20),
+	`description` NVARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `role` (`id`, `role`, `description`) VALUES
+('1','ADMIN', NULL),
+('2','TEACHER', NULL),
+('3','STUDENT', NULL);
+
+
+CREATE TABLE `user_role`(
+	`user_id` VARCHAR(20),
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	`role_id` VARCHAR(20),
+	FOREIGN KEY (`role_id`) REFERENCES `role`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(`user_id`, `role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `user_role`(`user_id`,`role_id`) VALUES
+('U00001','3'),
+('U00002','3'),
+('U00003','3'),
+('U00004','3'),
+('U00005','3'),
+('U00006','3'),
+('U00007','3'),
+('U00008','3'),
+('U00009','3'),
+('U00010','3'),
+('U00011','3'),
+('U00012','3'),
+('U00013','3'),
+('U00014','3'),
+('U00015','3');
+
+
+CREATE TABLE `semester`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`title` NVARCHAR(50),
+	`start_sem` DATE,
+	`end_sem` DATE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `semester`(`id`,`title`,`start_sem`,`end_sem`) VALUES
+('19201', 'HOC KY I 2019-2020',  '20191003', '20191007'),
+('19202', 'HOC KY II 2019-2020', '20191009', '20201001'), 
+('20211', 'HOC KY I 2020-2021', '20201003', '20201007');
+
+
+CREATE TABLE `subject`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`name` NVARCHAR(50),
+	`description` NVARCHAR(200),
+	`faculty_id` varchar(20),
+	FOREIGN KEY (`faculty_id`) REFERENCES `faculty`(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `subject`(`id`,`name`,`description`,`faculty_id`) VALUES
+('DASA230179', 'Cau truc du lieu va giai thuat', null, 'FIT'),
+('MATH132401', 'Toan 1', null, 'FAS'),
+('MATH132501', 'Toan 2', null, 'FAS'),
+('MATH143001', 'Dai so va cau truc dai so', null, 'FAS'),
+('MATH132901', 'Xac xuat thong ke va ung dung', null, 'FAS'),
+('PHYS130902', 'Ly 1', null, 'FAS'),
+('EEEN234162', 'Dien tu can ban', null, 'FEEE'),
+('INPR130285', 'Nhap mon lap trinh', null, 'FIT'),
+('PRTE230385', 'Ky thuat lap trinh', null, 'FIT'),
+('OOPR230279', 'Lap trinh huong doi tuong', null, 'FIT'),
+('INSE330380', 'An toan thong tin', null, 'FIT'),
+('WEPR330479', 'Lap trinh Web', null, 'FIT'),
+('SOEN330679', 'Cong nghe phan mem', null, 'FIT'),
+('NEES330380', 'Mang may tinh can ban', null, 'FIT'),
+('DBSY230184', 'Co so du lieu', null, 'FIT');
+
+
+CREATE TABLE `course`(
+	`id` VARCHAR(20) PRIMARY KEY,
+	`name` NVARCHAR(50),
+	`description` NVARCHAR(200),
+	`semester_id` VARCHAR(20),
+	FOREIGN KEY (`semester_id`) REFERENCES `semester`(`id`) ON UPDATE CASCADE ON DELETE SET NULL,
+	`subject_id` VARCHAR(20),
+	FOREIGN KEY (`subject_id`) REFERENCES `subject`(`id`) ON UPDATE CASCADE ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `course`(`id`,`name`,`description`,`semester_id`,`subject_id`) VALUES
+('MATH132501_20211_1', 'Toan 2', NULL, '20211', 'MATH132501'),
+('MATH143001_20211_1', 'Dai so va cau truc dai so', NULL, '20211', 'MATH143001'),
+('INSE330380_20211_1', 'An toan thong tin', NULL, '20211', 'INSE330380'),
+('WEPR330479_20211_1', 'Lap trinh Web', NULL, '20211', 'WEPR330479'),
+('SOEN330679_20211_1', 'Cong nghe phan mem', NULL, '20211', 'SOEN330679'),
+('DBSY230184_20211_1', 'Co so du lieu', NULL, '20211', 'DBSY230184'),
+('INPR130285_20211_1', 'Nhap mon lap trinh', NULL, '20211', 'INPR130285'),
+('EEEN234162_20211_1', 'Dien tu can ban', NULL, '20211', 'EEEN234162');
+
+
+CREATE TABLE `learns`(
+	`user_id` VARCHAR(20),
+	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	`course_id` VARCHAR(20),
+	FOREIGN KEY (`course_id`) REFERENCES `course`(`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY(`user_id`, `course_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `learns`(`user_id`,`course_id`) VALUES
+('U00002','MATH132501_20211_1'),
+('U00002','MATH143001_20211_1'),
+('U00002','WEPR330479_20211_1'),
+('U00002','SOEN330679_20211_1'),
+('U00002','INPR130285_20211_1'),
+('U00002','DBSY230184_20211_1'),
+('U00002','INSE330380_20211_1'),
+('U00002','EEEN234162_20211_1'),
+('U00005','MATH132501_20211_1'),
+('U00005','MATH143001_20211_1'),
+('U00005','WEPR330479_20211_1'),
+('U00005','SOEN330679_20211_1'),
+('U00005','INPR130285_20211_1'),
+('U00005','DBSY230184_20211_1'),
+('U00005','INSE330380_20211_1'),
+('U00005','EEEN234162_20211_1'),
+('U00004','MATH132501_20211_1'),
+('U00004','MATH143001_20211_1'),
+('U00004','WEPR330479_20211_1'),
+('U00004','SOEN330679_20211_1'),
+('U00004','INPR130285_20211_1'),
+('U00004','DBSY230184_20211_1'),
+('U00004','INSE330380_20211_1'),
+('U00004','EEEN234162_20211_1');
